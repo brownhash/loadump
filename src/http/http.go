220e5682478ade09451b69f3cec83c3a42f4f7c1
sharma1612harshit <sharma1612harshit@gmail.com>
2021-03-26 18:48:04 +0530

    add http package

---
+++ src/http/http.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"github.com/sharma1612harshit/loadump/src/logs"
+)
+
+func MakeRequest(method, url string, header map[string]string, body interface{}) (string, []byte, error) {
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(body)
+
+	client := &http.Client{}
+	request, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		logs.Error(err)
+	}
+
+	if len(header) > 0 {
+		for key, value := range header {
+			request.Header.Set(key, value)
+		}
+	}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	respBody, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	return response.Status, respBody, err
+}
