40a55c97c9c3c1be7d97502f58dfc99ffd845b4c
sharma1612harshit <sharma1612harshit@gmail.com>
2021-04-02 19:41:58 +0530

    use changed config type

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 	"github.com/sharma1612harshit/golog"
 )
 
-func ReadConfig(configFilePath string) config {
+func ReadConfig(configFilePath string) dumpConfig {
 	jsonFile, err := os.Open(configFilePath)
 	defer jsonFile.Close()
 
@@ -22,7 +22,7 @@ func ReadConfig(configFilePath string) config {
 		golog.Error(fmt.Sprintf("Unable to read config file. Error: %v", err))
 	}
 
-	configuration := config{}
+	configuration := dumpConfig{}
 
 	err = json.Unmarshal(byteValue, &configuration)
 
