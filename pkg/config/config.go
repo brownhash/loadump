e65b70fe70e560c61db2684c0707fd20d891abdd
sharma1612harshit <sharma1612harshit@gmail.com>
2021-04-02 19:38:50 +0530

    change indentation

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,17 +11,21 @@ import (
 func ReadConfig(configFilePath string) config {
 	jsonFile, err := os.Open(configFilePath)
 	defer jsonFile.Close()
+
 	if err != nil {
 		golog.Error(fmt.Sprintf("Unable to open config file. Error: %v", err))
 	}
 
 	byteValue, err := ioutil.ReadAll(jsonFile)
+
 	if err != nil {
 		golog.Error(fmt.Sprintf("Unable to read config file. Error: %v", err))
 	}
 
 	configuration := config{}
+
 	err = json.Unmarshal(byteValue, &configuration)
+
 	if err != nil {
 		golog.Error(fmt.Sprintf("Unable to parse config file. Error: %v", err))
 	}
