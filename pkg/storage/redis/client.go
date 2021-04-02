8bef09fec9b85d854730ffcb3c4c45a0c3e0fb4f
sharma1612harshit <sharma1612harshit@gmail.com>
2021-04-02 22:08:16 +0530

    make redis pkg

--- pkg/storage/redis/client.go
+++ pkg/storage/redis/client.go
@@ -1,10 +1,10 @@
-package storage 
+package redis 
 
 import (
     "github.com/go-redis/redis/v8"
 )
 
-func Redis(addr, password string, db int) *redis.Client {
+func Client(addr, password string, db int) *redis.Client {
 	client := redis.NewClient(&redis.Options{
         Addr:     addr,
         Password: password,
