50f2b5b023fefd9e9519a943ed923c04b4d6cf34
sharma1612harshit <sharma1612harshit@gmail.com>
2021-05-02 18:04:25 +0530

    generate unique id using system mac address

---
+++ pkg/system/uid.go
@@ -0,0 +1,39 @@
+package system
+
+import (
+	"net"
+	"bytes"
+	"github.com/sharma1612harshit/golog"
+)
+
+func GetUid() (uint64, error) {
+    interfaces, err := net.Interfaces()
+
+    if err != nil {
+		golog.Debug(err)
+        return uint64(0), err
+    }
+
+    for _, i := range interfaces {
+        if i.Flags&net.FlagUp != 0 && bytes.Compare(i.HardwareAddr, nil) != 0 {
+
+            // Skip locally administered addresses
+            if i.HardwareAddr[0]&2 == 2 {
+                continue
+            }
+
+            var mac uint64
+            for index, addr := range i.HardwareAddr {
+                if index >= 8 {
+                    break
+                }
+                mac <<= 8
+                mac += uint64(addr)
+            }
+
+            return mac, nil
+        }
+    }
+
+    return uint64(0), nil
+}
